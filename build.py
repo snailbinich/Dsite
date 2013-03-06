1aa941164007e2b02fecbf19a8b895d92465c969
snailbinich <snailbinich@gmail.com>
2013-03-06 23:15:49 +0800

    modify build script

--- build.py
+++ build.py
@@ -7,25 +7,54 @@ templateDir   = baseDir+os.sep+"template"
 sourcesDir    = baseDir+os.sep+"source"
 outputDir     = baseDir+os.sep+"output"
 resourcesDirs = ["css","img","js"]
+
+
+
 def clean ():
     print "[info]execute clean"
     if (os.path.exists(outputDir)) :
         print "[info]output dir exists"
         shutil.rmtree(outputDir)
         print "[info]deleting output dir"
+#calculate how many char in string
+def calculateNums(string, char):
+    num = 0;
+    for c in string :
+       if char == c :
+           num = num +1
+    return num
+
+def getBaseUrlByNums(num):
+    result = ""
+    if (num == 1) :
+        return "./" 
+    for i in range(1, num):
+        result += "../"
+    return result
 
 def copyDir(src, dest):
     shutil.copytree(src, dest)
 
 def complie(file):
-    print file
+    print "[info] compiling file :"+file
+    targetFile = file.replace(sourcesDir, outputDir).replace(".md",".html")
+    targetFileParentDir = os.path.dirname(targetFile)
+    print getBaseUrlByNums(calculateNums(file.replace(sourcesDir,""), "/"))
+    if not os.path.exists(os.path.dirname(targetFile)):
+        os.mkdir(targetFileParentDir)
+    cmd = "pandoc -s --template="+templateDir+"/template.html "
+    cmd += " -o "+targetFile+" "+file
+    print "[info] calling command : "+cmd
+    os.system(cmd)
 
 def walkDir(path):
+    print "[info] walking dir "+path
     for file in os.listdir(path):
-        if (os.path.isdir(file)):
-            walkDir(file)
-        else if (os.path.isfile(file)):
-            complie(file)
+        fullPath = path+os.sep+file
+        if (os.path.isdir(fullPath)):
+            walkDir(fullPath)
+        elif (os.path.isfile(fullPath)):
+            complie(fullPath)
 
 #remove output dir
 clean()
